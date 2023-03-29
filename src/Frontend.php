38ceb4a3cca24c9485847dcc2f1616b484f7b639
Robert Bokori <robert@wpcoders.io>
2023-03-30 00:57:03 +0200

    Html custom element conflict fix

--- src/Frontend.php
+++ src/Frontend.php
@@ -27,7 +27,7 @@ class Frontend
     public function wpHead()
     {
         if (empty($this->settings['gtm_id'])) {
-            return '';
+            return;
         }
 
         $datalayer_content = apply_filters('gds_cmp_datalayer', $this->getDataLayerData());
@@ -42,7 +42,7 @@ class Frontend
 
         <?php
         if (!empty($this->settings['container_off'])) {
-            return '';
+            return;
         }
         ?>
 
@@ -59,6 +59,10 @@ class Frontend
 
     public function wpFooter()
     {
+        if (empty($this->settings['gtm_id'])) {
+            return;
+        }
+
         if (!empty($this->settings['event_user_logged_in'])) {
             $user_logged_in = array_key_exists('genero_cmp_logged_in', $_COOKIE) ? filter_var(wp_unslash($_COOKIE['genero_cmp_logged_in']), FILTER_VALIDATE_INT) : 0;
 
@@ -381,92 +385,92 @@ class Frontend
         $hash = md5(json_encode($settings['consents']));
         $configs = esc_html(wp_json_encode($settings));
         $body = '
-    <modal-dialog
-        class="cookie-consent"
-        aria-labelledby="cc-heading"
-        aria-descibedby="cc-description"
-        persistent
-        scroll-lock
-        data-cookie-consent-hash="' . $hash . '"
-        data-configs="' . $configs . '"
-        >
-        <h2 id="cc-heading">' . __('Cookie Preferences', 'gds') . '</h2>
-        <p id="cc-description">
-        ' . __('We use cookies to provide a better user experience and personalised service. By consenting to the use of cookies, we can develop an even better service and will be able to provide content that is interesting to you. You are in control of your cookie preferences, and you may change them at any time. Read more about our cookies.') . '
-        </p>
-    ';
+            <gds-cmp-modal-dialog
+                class="cookie-consent"
+                aria-labelledby="cc-heading"
+                aria-descibedby="cc-description"
+                persistent
+                scroll-lock
+                data-cookie-consent-hash="' . $hash . '"
+                data-configs="' . $configs . '"
+                >
+                <h2 id="cc-heading">' . __('Cookie Preferences', 'gds') . '</h2>
+                <p id="cc-description">
+                ' . __('We use cookies to provide a better user experience and personalised service. By consenting to the use of cookies, we can develop an even better service and will be able to provide content that is interesting to you. You are in control of your cookie preferences, and you may change them at any time. Read more about our cookies.') . '
+                </p>
+            ';
 
         if (count($settings['consents']) > 0) {
             $body .= '
-        <div id="cookie-settings" class="cookie-consent__cookies">
+                <div id="cookie-settings" class="cookie-consent__cookies">
 
-        <gds-accordion>
-        ';
+                <gds-cmp-accordion>
+                ';
             foreach ($settings['consents'] as $consent) {
                 $body .= '
-            <gds-accordion-item>
-                <label slot="label">
-                    <input type="checkbox" name="cookie-consent" ' . (($consent['necessary']) ? 'required' : '') . ' ' . ((@$consent['consent']) ? 'checked disabled' : '') . ' value="' . $consent['id'] . '">
-                    ' . $consent['label'] . '
-                </label>
-                <i slot="icon" class="fa fa-solid fa-chevron-down"></i>
+                    <gds-cmp-accordion-item>
+                        <label slot="label">
+                            <input type="checkbox" name="cookie-consent" ' . (($consent['necessary']) ? 'required' : '') . ' ' . ((@$consent['consent']) ? 'checked disabled' : '') . ' value="' . $consent['id'] . '">
+                            ' . $consent['label'] . '
+                        </label>
+                        <i slot="icon" class="fa fa-solid fa-chevron-down"></i>
 
-                <p>' . $consent['description'] . '</p>
-            </gds-accordion-item>
-            ';
+                        <p>' . $consent['description'] . '</p>
+                    </gds-cmp-accordion-item>
+                    ';
             }
-            $body .= '
-        </gds-accordion>
-        </div>
+                    $body .= '
+                </gds-cmp-accordion>
+                </div>
         ';
         }
         $body .= '
-        <div class="wp-block-buttons cookie-consent__buttons">
-        <div class="wp-block-button is-style-outline" id="accept-selected-button">
-            <button
-            data-cookie-consent-accept-selected
-            class="wp-block-button__link"
-            >' . __('Accept selected cookies') . '</toggle-button>
-        </div>
+            <div class="wp-block-buttons cookie-consent__buttons">
+            <div class="wp-block-button is-style-outline" id="accept-selected-button">
+                <button
+                data-cookie-consent-accept-selected
+                class="wp-block-button__link"
+                >' . __('Accept selected cookies') . '</toggle-button>
+            </div>
 
-        <div class="wp-block-button is-style-outline">
-            <toggle-button
-            persistent
-            aria-controls="cookie-settings accept-selected-button"
-            class="wp-block-button__link"
-            >' . __('Edit cookie settings') . '</toggle-button>
-        </div>
+            <div class="wp-block-button is-style-outline">
+                <gds-cmp-toggle-button
+                persistent
+                aria-controls="cookie-settings accept-selected-button"
+                class="wp-block-button__link"
+                >' . __('Edit cookie settings') . '</gds-cmp-toggle-button>
+            </div>
 
-        <div class="wp-block-button">
-            <button
-            data-cookie-consent-accept-all
-            class="wp-block-button__link"
-            >' . __('Accept all cookies') . '</button>
-        </div>
-        </div>
-    </modal-dialog>';
+            <div class="wp-block-button">
+                <button
+                data-cookie-consent-accept-all
+                class="wp-block-button__link"
+                >' . __('Accept all cookies') . '</button>
+            </div>
+            </div>
+        </gds-cmp-modal-dialog>';
 
         $body .= "
-    <!-- Consent Manager by genero-cmp -->
-    <script>
-		window.dataLayer = window.dataLayer || []
-		// window.dataLayer = dataLayer
-		;(function() {
-		var consentManager = document.querySelector('.cookie-consent')
-		if (consentManager) {
-			// TCF v2 API present, now check if CMP is loaded
-			consentManager.addEventListener('accept', function(event) {
-                console.log(event)
-				// Push consent data to dataLayer for easy access in GTM.
-				window.dataLayer.push({
-					consentSetting: event.detail.settings,
-				})
-			})
-		}
-		})()
-    </script>
-	<!-- Consent Manager by genero-cmp -->
-    ";
+            <!-- Consent Manager by genero-cmp -->
+            <script>
+                window.dataLayer = window.dataLayer || []
+                // window.dataLayer = dataLayer
+                ;(function() {
+                var consentManager = document.querySelector('.cookie-consent')
+                if (consentManager) {
+                    // TCF v2 API present, now check if CMP is loaded
+                    consentManager.addEventListener('accept', function(event) {
+                        console.log(event)
+                        // Push consent data to dataLayer for easy access in GTM.
+                        window.dataLayer.push({
+                            consentSetting: event.detail.settings,
+                        })
+                    })
+                }
+                })()
+            </script>
+            <!-- Consent Manager by genero-cmp -->
+            ";
 
         echo $body;
     }
